649e1fa191e2920ea4441d60694fe315caa27dcb
lusiekseventeen <szymczuk.lukasz@gmail.com>
2017-12-04 22:38:01 +0100

    Rozbudowa modelu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import kivy
 from logFunc import logFunc
 from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.app import App
 from kivy.lang import Builder
@@ -8,19 +9,26 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.image import Image
 from kivy.properties import StringProperty
 from kivy.uix.screenmanager import WipeTransition
+from kivy.uix.listview import ListItemButton
 
 
 
 kivy.require('1.9.1')
-# You can create your kv code in the Python file
+
 Builder.load_file("res/ScreenLogin.kv")
 Builder.load_file("res/ScreenMainMid.kv")
 Builder.load_file("res/ScreenSettingLeft.kv")
 Builder.load_file("res/ScreenMatchesRight.kv")
+#Builder.load_file("res/MatchItem.kv")
 
 
-# Create a class for all screens in which you can include
-# helpful methods specific to that screen
+class MatchItem(BoxLayout):
+    def __init__(self, **kwargs):
+        super(ScreenMainMid, self).__init__(**kwargs)
+        self.current_photo = 0
+
+
+
 class ScreenLogin(Screen):
     def verify(self):
         if logFunc(password=self.ids.in_pass.text, username=self.ids.in_login.text, directory="assets/myfriends/"):
@@ -33,6 +41,9 @@ class ScreenLogin(Screen):
                           size_hint=(None, None), size=(200, 120))
             button.bind(on_press=popup.dismiss)
             popup.open()
+            screen_manager.transition = WipeTransition()
+            screen_manager.transition.duration = 1
+            screen_manager.current = 'screen_main_mid'
 
 
 class ScreenMainMid(Screen):
@@ -48,10 +59,19 @@ class ScreenMainMid(Screen):
         self.current_photo += 1
         return self.path + str(self.current_photo) + ".jpg"
 
+    def likeher(self):
+        matches.append(current_girl)
+        #nextGirl()
+        self.loadPicture()
+
     def loadPicture(self):
         self.ids.img.source = self.next_photo()
         self.ids.img.reload()
         self.do_layout()
+
+    #TODO: nastepna dziewucha zamiast nastepnego zdjecia.
+    #def nextGirl(self):
+        #current_girl =
 
 class ScreenSettingLeft(Screen):
     pass
@@ -59,6 +79,24 @@ class ScreenSettingLeft(Screen):
 class ScreenMatchesRight(Screen):
     pass
 
+class Girl():
+    id = 0
+    name = ""
+    photo_url = ""
+
+    def __init__(self,id, name, photo_url):
+        self.id = id
+        self.name = name
+        self.photo_url = photo_url
+
+#TODO Pobrane dziewuchy klasy Girls() wrzucic do tej listy
+all_my_girls = []
+#list tych dziewuch ktore sie podobaja
+matches = []
+
+test_girl = Girl(0, "Ala", "assets/myfriends/0.jpg")
+current_girl = test_girl
+matches.append(test_girl)
 
 
 screen_manager = ScreenManager()
@@ -68,6 +106,8 @@ screen_manager.add_widget(ScreenLogin(name="screen_login"))
 screen_manager.add_widget(ScreenMainMid(name="screen_main_mid"))
 screen_manager.add_widget(ScreenSettingLeft(name="screen_setting_left"))
 screen_manager.add_widget(ScreenMatchesRight(name="screen_matches_right"))
+
+
 
 
 class FacebookGirlsApp(App):
