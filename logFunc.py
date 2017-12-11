4589d47c72377b37225877afe1029b96030bef7e
lizzard888 <lizzard@autograf.pl>
2017-12-11 12:52:20 +0100

    Dodano dwie funkcje obsługujące machine learning.

--- logFunc.py
+++ logFunc.py
@@ -2,6 +2,7 @@ import facebook
 import requests
 import pyquery
 from urllib import request as urlrq
+import threading
 
 
 class Girl:
@@ -17,7 +18,7 @@ class Girl:
         self.path = "assets/myfriends/" + str(self.id) + ".jpg"
 
 
-def login(session, email, password, array):
+def login(session, email, password):
     response = session.get('https://m.facebook.com')
     response = session.post('https://m.facebook.com/login.php', data={
         'email': email,
@@ -25,34 +26,40 @@ def login(session, email, password, array):
     }, allow_redirects=False)
     if 'c_user' not in response.cookies:
         return False
-    else:
-        homepage_resp = session.get('https://m.facebook.com/home.php')
-        dom = pyquery.PyQuery(homepage_resp.text.encode('utf8'))
-        fb_dtsg = dom('input[name="fb_dtsg"]').val()
-        user_id = response.cookies['c_user']
-        xs = response.cookies['xs']
-        payload = {'grant_type': 'client_credentials', 'client_id': "2030671310550069", 'client_secret': "a497dfc6e769365ab08b93999c3ed76e"}
-        file = requests.post('https://graph.facebook.com/oauth/access_token?', params=payload)
-        token = file.json()['access_token']
-        graph = facebook.GraphAPI(token)
-        p = graph.get_connections(user_id, "picture?height=9000&redirect=false")
-        picture_id = p["data"]["url"].split("_")[1]
-        friends = graph.get_connections(picture_id, "likes?fields=link,name&limit=5000")
-        number = 0
-        for i in range(0, len(friends["data"])):
-            nodeid = friends["data"][i]["id"]
-            nodename = friends["data"][i]["name"]
-            nodelink = friends["data"][i]["link"]
-            username = nodelink[25:]
-            check = (username[:7])
-            if (check == "profile"):
-                continue
-            if nodename.split(" ")[0][-1] == 'a':
-                number += 1
-                pic = graph.get_connections(nodeid, "picture?height=9000&redirect=false")
-                new = Girl(id=number, name=nodename, photo_url=pic["data"]["url"])
-                array.append(new)
-        return True
+    return True
 
+def loading_photo(session, email, password, array):
+    response = session.get('https://m.facebook.com')
+    response = session.post('https://m.facebook.com/login.php', data={
+        'email': email,
+        'pass': password
+    }, allow_redirects=False)
+    homepage_resp = session.get('https://m.facebook.com/home.php')
+    dom = pyquery.PyQuery(homepage_resp.text.encode('utf8'))
+    fb_dtsg = dom('input[name="fb_dtsg"]').val()
+    user_id = response.cookies['c_user']
+    xs = response.cookies['xs']
+    payload = {'grant_type': 'client_credentials', 'client_id': "2030671310550069",
+               'client_secret': "a497dfc6e769365ab08b93999c3ed76e"}
+    file = requests.post('https://graph.facebook.com/oauth/access_token?', params=payload)
+    token = file.json()['access_token']
+    graph = facebook.GraphAPI(token)
+    p = graph.get_connections(user_id, "picture?height=9000&redirect=false")
+    picture_id = p["data"]["url"].split("_")[1]
+    friends = graph.get_connections(picture_id, "likes?fields=link,name&limit=5000")
+    number = 0
+    for i in range(0, len(friends["data"])):
+        nodeid = friends["data"][i]["id"]
+        nodename = friends["data"][i]["name"]
+        nodelink = friends["data"][i]["link"]
+        username = nodelink[25:]
+        check = (username[:7])
+        if (check == "profile"):
+            continue
+        if nodename.split(" ")[0][-1] == 'a':
+            pic = graph.get_connections(nodeid, "picture?height=9000&redirect=false")
+            new = Girl(id=number, name=nodename, photo_url=pic["data"]["url"])
+            array.append(new)
+            number += 1
 
 
